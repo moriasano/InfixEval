53d49194d3c2924b8172dc19ed64c55703f3f29b
mori asano <mori asano>
2016-12-07 16:10:11 -0500

    reorganization

--- InfixEval.scala
+++ InfixEval.scala
@@ -4,14 +4,30 @@
  ** COSC 455, Programming Assignment 2 **
  ****************************************/
 
-object InfixEval {
-  /** Takes infix an arithmetic expression from the user,  **
-   ** displays the S-Expression & numerical result         **/
+import scala.util.parsing.combinator.syntactical.StandardTokenParsers
+import scala.collection.mutable.Stack
 
-  // Constants
-  val c = scala.io.StdIn  // For increased readability
+trait constants {
+  /** Definitions used across both objects **/
+  val console = scala.io.StdIn  // For increased readability
   val numbers = for(i <- List.range(0, 10)) yield i.toString
   val symbols = List("+", "-", "*", "/", "^", ">", "<", "!", "(", ")")
+  def gcd(x: Int,y: Int): Int = y match {
+    /** Greatest Common Divisor **/
+
+    case 0 => x.abs
+    case _ => gcd(y, x % y)
+  }
+  def lcm(x: Int,y: Int): Int = {
+    /** Least Common Multiple**/
+
+    (x * y).abs / gcd(x, y)
+  }
+}
+
+object InfixEval extends constants {
+  /** Takes infix an arithmetic expression from the user,  **
+   ** displays the S-Expression & numerical result         **/
 
   def main(args: Array[String]): Unit = {
     /** Wrapper to provide looping **/
@@ -24,59 +40,25 @@ object InfixEval {
     /** Main Control Flow **/
 
     val infixExpression = getInfix()
-    val postfixExpression = InfixToRPN.ShuntingYard.convert(infixExpression)
+    val postfixExpression = RPN.ShuntingYard.convertFromInfix(infixExpression)
     // val sExpression = ...
-    val numericValue = rpnEval(postfixExpression)
+    val numericValue = RPN.ShuntingYard.eval(postfixExpression)
 
     println(infixExpression)
     println(postfixExpression)
     println(numericValue)
 
     // Tail recursion for looping
-    if(Array("yes", "y").contains(c.readLine("Again? (y/n)").toLowerCase)) { go() }
-  }
-
-  def rpnEval(rpnExpression: String): Int = {
-    /** Get the numeric result from the RPN expression **/
-
-    import scala.collection.mutable.Stack
-
-    val stack: Stack[Int] = new Stack[Int]
-    for (token <- rpnExpression.split(" ")) {
-      if (symbols.contains(token.toString)) token match {
-        case "+" => stack.push(stack.pop + stack.pop)
-        case "-" => val x = stack.pop; stack.push(stack.pop - x)
-        case "*" => stack.push(stack.pop * stack.pop)
-        case "/" => val x = stack.pop; stack.push(stack.pop / x)
-        case "^" => val x = stack.pop; stack.push(math.pow(stack.pop, x).toInt)
-        case ">" => stack.push(gcd(stack.pop, stack.pop))
-        case "<" => stack.push(lcm(stack.pop, stack.pop))
-        // case "!" =>
-      }
-      else stack.push(token.toInt)
-    }
-    stack.pop
-  }
-
-  def gcd(x: Int,y: Int): Int = y match {
-    /** Greatest Common Divisor **/
-
-    case 0 => x.abs
-    case _ => gcd(y, x % y)
-  }
-  def lcm(x: Int,y: Int): Int = {
-    /** Least Common Multiple**/
-
-    (x * y).abs / gcd(x, y)
+    if(Array("yes", "y").contains(console.readLine("Again? (y/n)").toLowerCase)) { go() }
   }
 
   def getInfix(): String = {
     /** Prompt the user for a string w/ input validation **/
 
-    val expression = c.readLine("Enter an expression: ")
+    val expression = console.readLine("Enter an expression: ")
 
-    // Check that every char is in 'numbers' or 'symbols'
-    if(!expression.forall(ch => (numbers ::: symbols).contains(ch.toString))) {
+    // Check that every character is in 'numbers' or 'symbols'
+    if(!expression.forall(c => (numbers ::: symbols).contains(c.toString))) {
       println("Invalid input. Try again.")
       getInfix()
     }
@@ -85,11 +67,9 @@ object InfixEval {
 }
 
 // TODO: factorial is not working
-object InfixToRPN {
-  /** Conversion from infix to RPN w/ Shunting algorithm          **
-   ** reference: blog.xebia.com/shuntingyard-algorithm-in-scala/  **/
-
-  import scala.util.parsing.combinator.syntactical.StandardTokenParsers
+object RPN extends constants {
+  /** Grammar definition for expression in Reverse Polish Notation    **
+   ** reference: blog.xebia.com/shuntingyard-algorithm-in-scala/      **/
 
   abstract class Expression { def rpnExpression: String }
   case class BinaryOperator(left: Expression, operation: String, right: Expression) extends Expression {
@@ -101,6 +81,8 @@ object InfixToRPN {
   case class Number(value: String) extends Expression { def rpnExpression: String = value }
 
   object ShuntingYard extends StandardTokenParsers {
+    /** Various methods for creation / conversion of RPN expression **/
+
     lexical.delimiters ++= InfixEval.symbols
 
     // Infix Expression Grammar
@@ -123,7 +105,7 @@ object InfixToRPN {
       phrase(expression)(tokens)
     }
 
-    def convert(expressionString: String): String = expressionString match {
+    def convertFromInfix(expressionString: String): String = expressionString match {
       case null => ""
       case ""   => ""
       case _    =>
@@ -131,6 +113,34 @@ object InfixToRPN {
           case Success(tree, _) => tree.rpnExpression
           case e: NoSuccess     => e.toString
         }
+    }
+    def eval(rpnExpression: String): Int = {
+      /** Get the numeric result from the RPN expression **/
+
+      val stack: Stack[Int] = new Stack[Int]
+      for (token <- rpnExpression.split(" ")) {
+        if (InfixEval.symbols.contains(token.toString)) token match {
+          case "+" => stack.push(stack.pop + stack.pop)
+          case "-" => val x = stack.pop; stack.push(stack.pop - x)
+          case "*" => stack.push(stack.pop * stack.pop)
+          case "/" => val x = stack.pop; stack.push(stack.pop / x)
+          case "^" => val x = stack.pop; stack.push(math.pow(stack.pop, x).toInt)
+          case ">" => stack.push(gcd(stack.pop, stack.pop))
+          case "<" => stack.push(lcm(stack.pop, stack.pop))
+          // case "!" =>
+        }
+        else stack.push(token.toInt)
+      }
+      stack.pop
+    }
+    def toPolish(rpnExpression: String): String = {
+      /** Convert the RPN to Polish (Prefix notation) **/
+
+      val stack: Stack[String] = new Stack[String]
+      for (token <- rpnExpression.split(" ")) {
+
+      }
+
     }
   }
 }
